49c0293a4b62e32e8401927db1e50b88a55adaf7
atlir <dmiriy.spirin@gmail.com>
2020-05-18 15:15:59 +0300

    Add text question component styles

--- src/components/SurveyComponent/SurveyQuestions/SurveyTextQuestion.js
+++ src/components/SurveyComponent/SurveyQuestions/SurveyTextQuestion.js
@@ -1,13 +1,39 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const SurveyRadioQuestion = (props) => {
-    const { question, onSelect } = props;
-    const [answer, setAnswer] = useState();
+  const { questionID, onSelect, required } = props;
+
+  const [answer, setAnswer] = useState('');
+  const [error, setError] = useState(false);
+
+  const onClickHandler = () => {
+    if(required && answer.length === 0){
+      setError(true);
+      return;
+    }
+
+    return onSelect(answer)
+  }
+
+  const onInputHandler = (event) => {
+    if(error){
+      setError(false);
+    }
+
+    setAnswer(event.target.value)
+  }
 
   return (
-    <div className="survey-widget__question-wrapper survey-widget__radio-wrapper">
-        <textarea resize="none" id={question.id} name={question.id} onInput={setAnswer}>{answer}</textarea>
-        <button onClick={onSelect(answer)}>Next</button>
+    <div className="survey-widget__question-wrapper survey-widget__text-wrapper">
+      <textarea
+        className={`survey-widget__textarea ${error ? 'survey-widget__textarea--error' : ''}`}
+        resize="none"
+        id={questionID}
+        name={questionID}
+        onChange={onInputHandler}
+        value={answer}
+      />
+      <button className="survey-widget__button" onClick={onClickHandler}>Next</button>
     </div>
   );
 };
